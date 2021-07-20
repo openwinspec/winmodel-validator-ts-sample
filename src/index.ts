d695bb2e6cf397138d74d2750503e082536a70e6
Dmitry Bratsun <dmitry.bratsun@gmail.com>
2021-07-20 10:01:21 +0300

    Import winmodel-validator-ts module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-export const greet = (name: string): string => `Hello ${name}`;
+import { validate } from 'winmodel-validator-ts';
+const fs = require('fs');
 
-console.log(greet('world'));
+let rawdata = fs.readFileSync('./src/sample.model.json');
+
+console.time('valadate time: ');
+
+validate(rawdata)
+  .then((result) => {
+    console.log('Success');
+  })
+  .catch((error) => {
+    console.log('Error: ', error);
+  })
+  .finally(() => {
+    console.timeEnd('valadate time: ');
+  });
